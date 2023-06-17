5a1862ff4cc9bac8b592d3f4686b0fd84169856f
Marco <marcokammer59@gmail.com>
2023-06-17 21:35:43 +0300

    test5

--- lua/poimandres/colors.lua
+++ lua/poimandres/colors.lua
@@ -5,65 +5,43 @@ local M = {}
 
 ---@class Palette
 M.default = {
-    brightYellow = '#FFFAC2',
-    brightMint = '#5DE4C7',
-    lowerMint = '#5FB3A1',
-    bluishGreen = '#42675A',
-    lowerBlue = '#89DDFF',
-    lightBlue = '#ADD7FF',
-    desaturatedBlue = '#91B4D5',
-    BluishGrayBrighter = '#7390AA',
-    hotRed = '#D0679D',
-    pink = '#F087BD',
-    gray = '#A6ACCD',
-    darkerGray = '#767C9D',
+    brightYellow = '#fffac2',
+    brightMint = '#5DE4c7',
+    lowerMint = '#5fb3a1',
+    bluishGreen = '#42675a',
+    lowerBlue = '#89ddff',
+    lightBlue = '#add7ff',
+    desaturatedBlue = '#91b4d5',
+    BluishGrayBrighter = '#7390aa',
+    hotRed = '#d0679d',
+    pink = '#f087bd',
+    gray = '#a6accd',
+    darkerGray = '#767c9d',
     bluishGray = '#506477',
     focus = '#303340',
-    bg = '#1B1E28',
-    offWhite = '#E4F0FB',
+    bg = '#1b1e28',
+    offWhite = '#e4f0fb',
     selection = '#717cb4',
-    white = '#FFFFFF',
+    white = '#ffffff',
     black = '#000000',
     none = 'NONE',
-    git = { add = "#FFFAC2", change = "#ADD7FF", delete = "#D0679D" },
+    git = { add = "#fffac2", change = "#add7ff", delete = "#d0679d" },
     gitSigns = {
-        add = "#FFFAC2",
-        change = "#ADD7FF",
-        delete = "#D0679D"
+        add = "#fffac2",
+        change = "#add7ff",
+        delete = "#d0679d"
     },
 }
-
-M.white = M.default
 
 M.storm = {
-    -- storm palette
-    brightYellow = '#FFFAC2',
-    brightMint = '#5DE4C7',
-    lowerMint = '#5FB3A1',
-    bluishGreen = '#42675A',
-    lowerBlue = '#89DDFF',
-    lightBlue = '#ADD7FF',
-    desaturatedBlue = '#91B4D5',
-    BluishGrayBrighter = '#7390AA',
-    hotRed = '#D0679D',
-    pink = '#F087BD',
-    gray = '#A6ACCD',
-    darkerGray = '#868CAD',
+    darkerGray = '#868cad',
     bluishGray = '#607487',
-    focus = '#404350',
-    bg = '#252B37',
+    bg = '#252b37',
     offWhite = '#e4f0fb',
-    selection = '#818CC4',
-    white = '#FFFFFF',
+    selection = '#818cc4',
     black = '#101010',
-    none = 'NONE',
-    git = { add = "#5DE4C7", change = "#ADD7FF", delete = "#D0679D" },
-    gitSigns = {
-        add = "#5DE4C7",
-        change = "#ADD7FF",
-        delete = "#D0679D"
-    },
 }
+M.white = M.storm
 
 
 ---@return ColorScheme
